704b6a6ef38c5f7f114579d4fb6c5f9b4e2e2ba1
leisenfelder <leisenfelder@gmail.com>
2018-03-11 13:27:33 +0900

    up date build: Mar 11

---
+++ build/CMakeFiles/SimpleWin.dir/DependInfo.cmake
@@ -0,0 +1,38 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/msys64/home/herb/bin/LearnGL/MyOpenGL_temp/lib/glfw/deps/getopt.c" "C:/msys64/home/herb/bin/LearnGL/MyOpenGL_temp/build/CMakeFiles/SimpleWin.dir/lib/glfw/deps/getopt.c.obj"
+  "C:/msys64/home/herb/bin/LearnGL/MyOpenGL_temp/lib/glfw/deps/glad.c" "C:/msys64/home/herb/bin/LearnGL/MyOpenGL_temp/build/CMakeFiles/SimpleWin.dir/lib/glfw/deps/glad.c.obj"
+  "C:/msys64/home/herb/bin/LearnGL/MyOpenGL_temp/lib/glfw/deps/tinycthread.c" "C:/msys64/home/herb/bin/LearnGL/MyOpenGL_temp/build/CMakeFiles/SimpleWin.dir/lib/glfw/deps/tinycthread.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../lib/glfw"
+  "../lib/glfw/deps"
+  "../lib/glfw/include"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/msys64/home/herb/bin/LearnGL/MyOpenGL_temp/src/main.cpp" "C:/msys64/home/herb/bin/LearnGL/MyOpenGL_temp/build/CMakeFiles/SimpleWin.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../lib/glfw"
+  "../lib/glfw/deps"
+  "../lib/glfw/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "C:/msys64/home/herb/bin/LearnGL/MyOpenGL_temp/build/lib/glfw/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/SimpleWin.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SimpleWin.dir/src/main.cpp.o"
+  "CMakeFiles/SimpleWin.dir/lib/glfw/deps/tinycthread.c.obj"
+  "CMakeFiles/SimpleWin.dir/lib/glfw/deps/getopt.c.obj"
+  "CMakeFiles/SimpleWin.dir/lib/glfw/deps/glad.c.obj"
+  "SimpleWin.pdb"
+  "SimpleWin.exe"
+  "SimpleWin.exe.manifest"
+  "libSimpleWin.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/SimpleWin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
